54d3f91eebac81ea68239b07d99914a0362960db
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-19 12:21:26 -0700

    1-printf

--- 0-get_print_func.c
+++ 0-get_print_func.c
@@ -1,1 +1,27 @@
+#include "main.h"
+/**
+ * get_print_func - selects the function for a specifier
+ * @s: specifier
+ * Return: a pointer to function
+ */
+int (*get_print_function(char s)(va_list))
+{
+	int i;
+
+	format_t formats[] = {
+	{'c', print_char},
+ 	{'s', print_string},
+	{'%', print_percent},
+	{0, NULL},
+	};
+
+	for (i = 0; formats[i].specifier; i++)
+	{
+		if (formats[i].specifier == s)
+		{
+			return formats[i].f;
+		}
+	}
+	return (NULL);
+}
 
---
+++ 1-printf_functions.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_char - prints a char
+ * @args: arguements
+ * Return: 1
+ */
+int print_char(va_list args)
+{
+	char c;
+	
+	c = (char)va_arg(args, int);
+	putchar(c);
+	return (1);
+}
+/**
+ * print_string - prints a string
+ * @args: argument
+ * Return: lenght of the string
+ */
+int print_string(va_list args)
+{
+	const char *str;
+	int count;
+	int i;
+
+	str = va_args(args, const char *);
+	for (i = 0; str[i]; i++)
+	{
+		putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_percent - prints '%' specifier
+ * @args: arguements
+ * Return: 1
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	putchar('%');
+	return (1);
+}
